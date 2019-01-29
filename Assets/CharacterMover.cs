030b2ce0004aec4252d7e0247e65b7f9372abad8
Keegan-NewDesktop <keeganland@gmail.com>
2019-01-28 21:03:56 -0800

    Initial push - Basic Unity scene created with capsule player character and npc + canvas to display dialog on

---
+++ Assets/CharacterMover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMover : MonoBehaviour
+{
+    public float moveSpeedf = 1.0f;
+
+    public void MoveCharacter(float x, float z)
+    {
+        Vector3 dirVector = new Vector3(x, 0, z).normalized * moveSpeedf;
+        GetComponent<Rigidbody>().MovePosition(transform.position + dirVector * Time.deltaTime);
+    }
+}
---
+++ Assets/PlayerInput.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    private CharacterMover playerMover;
+    [SerializeField]
+    private float xMovementf = 0.0f, zMovementf = 0.0f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        playerMover = GetComponent<CharacterMover>();
+    }
+
+
+
+    // Update is for scanning for inputs
+    void Update()
+    {
+
+        xMovementf = Input.GetAxisRaw("Horizontal");
+        zMovementf = Input.GetAxisRaw("Vertical");
+
+    }
+
+    // FixedUpdate is for passing inputs along to the controller script. 
+    private void FixedUpdate()
+    {
+
+        playerMover.MoveCharacter(xMovementf, zMovementf);
+    }
+}
